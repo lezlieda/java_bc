ce9cc949fc7b4222199c5198196948cc9e67f689
lezlieda <lezlieda@student.21-school.ru>
2024-09-14 18:50:12 +0300

    day05ex02 fix

--- Java_Bootcamp.Day05/src/ex02/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepository.java
+++ Java_Bootcamp.Day05/src/ex02/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepository.java
@@ -5,6 +5,7 @@ import edu.school21.chat.models.Message;
 import java.util.Optional;
 
 public interface MessagesRepository {
-    Optional<Message> findById(long id);
+    Optional<Message> findById(Long id);
+
     void save(Message message);
 }
