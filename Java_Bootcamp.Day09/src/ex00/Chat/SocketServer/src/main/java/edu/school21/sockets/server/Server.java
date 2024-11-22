39ac5a3af79e416a993a71f0a882a9d17b6b7452
lezlieda <lezlieda@student.21-school.ru>
2024-11-22 17:50:34 +0300

    ex00 returned to the old (blocking) java i/o

--- Java_Bootcamp.Day09/src/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/Server.java
+++ Java_Bootcamp.Day09/src/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/server/Server.java
@@ -1,71 +1,68 @@
 package edu.school21.sockets.server;
 
 import edu.school21.sockets.services.UsersService;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.socket.oio.OioServerSocketChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import io.netty.channel.oio.OioEventLoopGroup;
-
-import java.net.InetSocketAddress;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-//@Component
-//@Qualifier("server")
-//public class Server {
-//    private final UsersService usersService;
-//    static ExecutorService executorService = Executors.newFixedThreadPool(10);
-//
-//    @Autowired
-//    public Server(@Qualifier("usersService") UsersService usersService) {
-//        this.usersService = usersService;
-//    }
-//
-//    public void start(int port) {
-//        try {
-//            ServerSocket server = new ServerSocket(port);
-//            while (!server.isClosed()) {
-//                Socket client = server.accept();
-//                executorService.execute(new ClientHandler(client, usersService));
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
 
 @Component
 @Qualifier("server")
 public class Server {
     private final UsersService usersService;
 
+
     @Autowired
-    public Server(@Qualifier("usersService")UsersService usersService) {
+    public Server(@Qualifier("usersService") UsersService usersService) {
         this.usersService = usersService;
     }
 
     public void start(int port) {
-        EventLoopGroup group = new OioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(group)
-                    .channel(OioServerSocketChannel.class)
-                    .localAddress(new InetSocketAddress(port))
-                    .childHandler(new ServerInitializer(usersService));
-            ChannelFuture f = b.bind().sync();
-            f.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
+            ServerSocket server = new ServerSocket(port);
+            Socket client = server.accept();
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            out.writeUTF("Hello from server!");
+            out.flush();
+            while (!client.isClosed()) {
+                String entry = in.readUTF();
+                if (entry.equals("signUp")) {
+                    signUp(in, out);
+                    break;
+                } else {
+                    out.writeUTF("Unknown command");
+                    out.flush();
+                }
+            }
+            client.close();
+            server.close();
+            in.close();
+            out.close();
+        } catch (Exception e) {
             throw new RuntimeException(e);
-        } finally {
-            group.shutdownGracefully();
         }
     }
+
+    public void signUp(DataInputStream in, DataOutputStream out) throws IOException {
+        out.writeUTF("Enter username:");
+        out.flush();
+        String username = in.readUTF();
+        out.writeUTF("Enter password:");
+        out.flush();
+        String password = in.readUTF();
+        if (usersService.signUp(username, password)) {
+            out.writeUTF("Successful!");
+            out.flush();
+        } else {
+            out.writeUTF("User not created");
+            out.flush();
+        }
+    }
+
 }
